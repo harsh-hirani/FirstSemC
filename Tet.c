1e9ec380d1dfb8b1ea8ce76ff53a46d997dde720
Harsh Hirani <hiraniharsh7@gmail.com>
2023-01-12 21:00:55 +0530

    shxsahxj

--- Tet.c
+++ Tet.c
@@ -1,1 +1,18 @@
-#include<stdio.h>
+
+// Online C compiler to run C program online
+#include <stdio.h>
+float rec(float n){
+    if (n==0){
+        return 0;
+    }else if(n==1){
+        return 1;
+    }
+    
+    return rec(n-1)+rec(n-2);
+}
+int main() {
+    // Write C code here
+    printf("Hello world\n\t%f",rec(9));
+
+    return 0;
+}
