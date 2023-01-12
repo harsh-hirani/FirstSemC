13431ff88a2922d70fea81332bbc9b2a5acaba92
Harsh Hirani <hiraniharsh7@gmail.com>
2023-01-12 21:21:25 +0530

    fibo

--- Tet.c
+++ Tet.c
@@ -1,15 +1,20 @@
 // Online C compiler to run C program online
 #include <stdio.h>
-int rec(int n){
-    if (n<2){
+float rec(float n){
+    if (n==0){
+        return 0;
+    }else if(n==1){
         return 1;
     }
     
-    return n*rec(n-1);
+    return rec(n-1)+rec(n-2);
 }
 int main() {
     // Write C code here
-    printf("Hello world\n\t%d",rec(6));
+    int u,i;
+    // printf("Hello world\n\t%f",rec(10));
+    scanf("%d",&u);
+    for(i=1;i<=u;printf("\n\t%d \t%f",i-1,rec(i++)));
 
     return 0;
 }
